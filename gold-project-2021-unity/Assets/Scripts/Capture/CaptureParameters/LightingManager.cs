d9d94fe6063355abe21c0b712b594f5b812c1d48
Evan Griffiths <evangriffiths@hotmail.co.uk>
2021-01-19 17:51:38 +0000

    🚧🔨 Materials are now stored in MaterialPairs in LightingManager (one lit, one unlit), custom editor to make populating these materials easy

--- gold-project-2021-unity/Assets/Scripts/Capture/CaptureParameters/LightingManager.cs
+++ gold-project-2021-unity/Assets/Scripts/Capture/CaptureParameters/LightingManager.cs
@@ -4,13 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+[System.Serializable]
+public class MaterialPair
+{
+    public Material LitMaterial;
+    public Material UnlitMaterial;
+}
+
 public class LightingManager : MonoBehaviour
 {
     private static Shader Lit;
     private static Shader Unlit;
     
-    [SerializeField] private Material[] Materials;
-
+    [SerializeField] private MeshRenderer[] Renderers;
+    [SerializeField] private MaterialPair[] MaterialPairs;
+    
     private bool IsLit = true;
     
     private void Awake()
@@ -22,18 +30,41 @@ public class LightingManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < Materials.Length; i++)
-        {
-            Materials[i].shader = Lit;
-            var litMaterial = new Material(Materials[i]);
-            
-            Materials[i].SetColor("_UnlitColor", litMaterial.color);
-            Materials[i].SetTexture("_UnlitColorMap",  litMaterial.mainTexture);
-            Materials[i].SetTextureScale("_UnlitColorMap", litMaterial.mainTextureScale);
-            Materials[i].SetTextureOffset("_UnlitColorMap", litMaterial.mainTextureOffset);
-        }
+        // for (int i = 0; i < Materials.Length; i++)
+        // {
+        //     Materials[i].shader = Lit;
+        //     var litMaterial = new Material(Materials[i]);
+        //
+        //     Materials[i].SetColor("_UnlitColor", litMaterial.color);
+        //     Materials[i].SetTexture("_UnlitColorMap",  litMaterial.mainTexture);
+        //     Materials[i].SetTextureScale("_UnlitColorMap", litMaterial.mainTextureScale);
+        //     Materials[i].SetTextureOffset("_UnlitColorMap", litMaterial.mainTextureOffset);
+        // }
     }
+    
 
+    public void SetMaterials(Material[] mats, bool isLit) 
+    {
+        var newPairs = new MaterialPair[mats.Length];
+
+        for (int i = 0; i < newPairs.Length; i++)
+        {
+            newPairs[i] = new MaterialPair();
+
+            MaterialPair currentPair = null;
+            
+            if (i < MaterialPairs.Length)
+            {
+                currentPair = MaterialPairs[i];
+            }
+            
+            newPairs[i].LitMaterial = isLit ? mats[i] : currentPair?.LitMaterial;
+            newPairs[i].UnlitMaterial = isLit ?  currentPair?.UnlitMaterial : mats[i];
+        }
+
+        MaterialPairs = newPairs;
+    }
+    
     public void SetLit(bool isLit)
     {
         if (IsLit == isLit)
@@ -42,9 +73,9 @@ public class LightingManager : MonoBehaviour
         }
         
         IsLit = isLit;
-        for (int i = 0; i < Materials.Length; i++)
-        {
-            Materials[i].shader = IsLit ? Lit : Unlit;
-        }
+        // for (int i = 0; i < Materials.Length; i++)
+        // {
+        //     Materials[i].shader = IsLit ? Lit : Unlit;
+        // }
     }
 }
---
+++ gold-project-2021-unity/Assets/Scripts/Editor/LightingManagerEditor.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// Code here is a modified version of code written here:
+// Live: https://gist.github.com/bzgeb/3800350
+// Archived: https://archive.is/Sc6kY
+
+[CustomEditor(typeof(LightingManager))]
+public class LightingManagerEditor : Editor
+{
+    private SerializedObject obj;
+
+    public void OnEnable ()
+    {
+        obj = new SerializedObject (target);
+    }
+ 
+    public override void OnInspectorGUI ()
+    {
+        if (DropAreaGUI("Drop Lit Materials Here", out var litMaterials))
+        {
+            // Populate lit materials
+            PopulateMaterials(litMaterials, isLit: true);
+        }
+        
+        EditorGUILayout.Space();
+        
+        if (DropAreaGUI("Drop Unlit Materials Here", out var unlitMaterials))
+        {
+            // Populate unlit materials
+            PopulateMaterials(unlitMaterials, isLit: false);
+        }
+        
+        EditorGUILayout.Space ();
+
+        DrawDefaultInspector ();
+    }
+
+    void PopulateMaterials(Object[] materialObjects, bool isLit)
+    {
+        var lighting = target as LightingManager;
+        if (lighting == null)
+        {
+            return;
+        }
+        
+        var materials = new List<Material>();
+        for (int i = 0; i < materialObjects.Length; i++)
+        {
+            var material = materialObjects[i] as Material;
+            if (material == null)
+            {
+                continue;
+            }
+            
+            materials.Add(material);
+        }
+        
+        lighting.SetMaterials(materials.ToArray(), isLit);
+    }
+    
+    public bool DropAreaGUI (string text, out Object[] droppedObjs)
+    {
+        droppedObjs = null;
+        
+        Event evt = Event.current;
+        Rect drop_area = GUILayoutUtility.GetRect (0.0f, 50.0f, GUILayout.ExpandWidth (true));
+        GUI.Box (drop_area, text);
+     
+        switch (evt.type) {
+            case EventType.DragUpdated:
+            case EventType.DragPerform:
+                if (!drop_area.Contains (evt.mousePosition))
+                    return false;
+             
+                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+         
+                if (evt.type == EventType.DragPerform) {
+                    DragAndDrop.AcceptDrag ();
+                    droppedObjs = DragAndDrop.objectReferences;
+                    return true;
+                }
+                break;
+        }
+
+        return false;
+    }
+}
